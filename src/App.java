ee4dc6d5b58728ff604137c319c5eaf2fbcfbbba
NickTriance <114621135+NickTriance@users.noreply.github.com>
2023-09-07 15:45:32 -0700

    Drag and drop kinda works, help menu added, merging files works

--- src/App.java
+++ src/App.java
@@ -2,11 +2,14 @@ import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import java.awt.event.KeyEvent;
+import java.io.IOException;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.InputEvent;
 
 import java.util.ArrayList;
@@ -15,13 +18,13 @@ import java.util.Hashtable;
 //TODO: Javadocs.
 //TODO: display something when no files are open
 //TODO: implement drag and drop in this view
-//TODO: set windows look and feel
 public class App {
 
     //Things it would be useful to have global access to
     private JFrame frame;
 
-    private Hashtable<String, FilePanel> fileHashtable;
+    private Hashtable<String, FilePanel> fileHashtable; //for storing files and their panels.
+    private ArrayList<String> fileList; //for keeping track of the order of files in the merger.
     
     public App() {
         //set look and feel by platform.
@@ -34,6 +37,7 @@ public class App {
         frame = createFrame();
 
         fileHashtable = new Hashtable<String, FilePanel>();
+        fileList = new ArrayList<String>();
 
         frame.setVisible(true);
     }
@@ -53,42 +57,51 @@ public class App {
         //frame.add(contentPanel);
         frame.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
 
+        frame.setTransferHandler(new TransferHandler("text") {
+            @Override
+            public boolean canImport(TransferHandler.TransferSupport support) {
+                return true;
+            }
+
+            @Override
+            public boolean importData(TransferHandler.TransferSupport support) {
+                try {
+                    String data = (String) support.getTransferable().getTransferData(DataFlavor.stringFlavor);
+                    
+                    //logic to rearrange filepanels based on data goes here
+
+                    refreshFrame();
+                    return true;
+                } catch (UnsupportedFlavorException | IOException e) {
+                    return false;
+                }
+            }
+        });
+
         return frame;
     }
 
     private JMenuBar createJMenuBar() {
         JMenuBar jmb = new JMenuBar();
         jmb.add(createFileMenu());
+        jmb.add(createHelpMenu());
         return jmb;
     }
 
+    /**
+     * Creates the File menu on the menu bar.
+     * @return FileMenu: JMenu
+     */
     private JMenu createFileMenu() {
         JMenu jmFile = new JMenu("File");
         jmFile.setMnemonic('F');
 
+        //create the menu for opening files
         JMenuItem jmiOpen = new JMenuItem("Open");
         jmiOpen.setMnemonic('O');
         jmiOpen.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_DOWN_MASK));
         jmiOpen.addActionListener((ae) -> {
-        
-            //create file chooser
-            JFileChooser fileChooser = new JFileChooser();
-            fileChooser.setAcceptAllFileFilterUsed(false);
-            FileNameExtensionFilter pdfFilter = new FileNameExtensionFilter("PDF Documents", "pdf");
-            fileChooser.setFileFilter(pdfFilter);
-
-            int returnVal = fileChooser.showOpenDialog(frame);
-            if (returnVal == JFileChooser.APPROVE_OPTION) {
-                String filePath = fileChooser.getSelectedFile().getAbsolutePath();
-
-                String fileName = fileChooser.getSelectedFile().getName();
-                FilePanel filePanel = new FilePanel(fileName);
-
-                fileHashtable.put(filePath, filePanel);
-                frame.add(filePanel);
-                frame.revalidate();
-                frame.repaint();
-            }
+            addFile();
         });
         jmFile.add(jmiOpen);
 
@@ -97,13 +110,74 @@ public class App {
         jmiSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
         jmiSave.addActionListener((ae) -> {
             //TODO: This is the part where the PDFS will be combined and saved
+            mergeFiles();
         });
         jmFile.add(jmiSave);
-
 
         return jmFile;
     }
 
+    private JMenu createHelpMenu() {
+        JMenu jmHelp = new JMenu("Help");
+        jmHelp.setMnemonic('H');
+
+        JMenuItem jmiHelp = new JMenuItem("View Help");
+        jmiHelp.setMnemonic('H');
+        jmiHelp.addActionListener((ae) -> {
+            JOptionPane.showMessageDialog(frame, "Add PDF files with File -> Open. \nArrange with drag and drop. \nExport with File -> Save/Export.", "Help", JOptionPane.INFORMATION_MESSAGE);
+        });
+        jmHelp.add(jmiHelp);
+
+        jmHelp.addSeparator();
+
+        JMenuItem jmiAbout = new JMenuItem("About...");
+        jmiAbout.setMnemonic('A');
+        jmiAbout.addActionListener((ae) -> {
+            JOptionPane.showMessageDialog(frame, "PDFMerger V0.1 by NickTriance\n www.nicktriance.com", "About", JOptionPane.INFORMATION_MESSAGE);
+        });
+        jmHelp.add(jmiAbout);
+
+        return jmHelp;
+    }
+    private JPopupMenu createPopupMenu() { 
+        JPopupMenu _jpm = new JPopupMenu(null);
+       
+        JMenuItem jpmDel = new JMenuItem("Delete");
+        jpmDel.setMnemonic('D');
+        jpmDel.addActionListener((ae) -> {
+            //todo: remove file
+        });
+
+        return _jpm;
+    }
+
+    /**Adds files to the file list. */
+    private void addFile() {
+        
+        //create file chooser
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setAcceptAllFileFilterUsed(false);
+        FileNameExtensionFilter pdfFilter = new FileNameExtensionFilter("PDF Documents", "pdf");
+        fileChooser.setFileFilter(pdfFilter);
+        //open the file chooser
+        int returnVal = fileChooser.showOpenDialog(frame);
+        //add the file
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            String filePath = fileChooser.getSelectedFile().getAbsolutePath();
+            String fileName = fileChooser.getSelectedFile().getName();
+            FilePanel filePanel = new FilePanel(fileName);
+            //ensure that we don't open duplicates.
+            if (fileList.contains(filePath)) {
+                JOptionPane.showMessageDialog(frame, "Selected file is already open.", "Error", JOptionPane.ERROR_MESSAGE);
+                return;
+            } 
+            fileHashtable.put(filePath, filePanel);
+            fileList.add(filePath);
+            frame.add(filePanel);
+            refreshFrame();
+        }
+    }
+    
     /**
      * Removes files from the file list
      * @param file : String, the file to be removed.
@@ -114,6 +188,21 @@ public class App {
         frame.remove(_panel);
         refreshFrame();
         fileHashtable.remove(_filepath);
+        fileList.remove(_filepath);
+    }
+
+    private void mergeFiles() {
+        String[] files = fileList.toArray(new String[0]);
+        JFileChooser fileChooser = new JFileChooser();
+        int saveChoice = fileChooser.showSaveDialog(frame);
+        if (saveChoice == JFileChooser.APPROVE_OPTION) {
+            try {
+                PDFMerger.merge(files, fileChooser.getSelectedFile().getAbsolutePath());
+                JOptionPane.showMessageDialog(frame, "Done", null, JOptionPane.INFORMATION_MESSAGE);
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(frame, "Failed to save file " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
     }
 
     private void refreshFrame() {
--- src/FilePanel.java
+++ src/FilePanel.java
@@ -1,10 +1,16 @@
 import javax.swing.*;
 import java.awt.GridLayout;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class FilePanel extends JPanel {
 
+    private static final int GRID_CELL_SIZE = 500; //size of the grid for drag and drop
+
     private JLabel fileLabel;
     private String fileName;
+    private Point mouseOffset;
     private String path;
 
 
@@ -18,6 +24,29 @@ public class FilePanel extends JPanel {
         fileName = parseFileName(filename);
         fileLabel = new JLabel(fileName);
         add(fileLabel);
+
+        //add a mouse listener for drag and drop
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                mouseOffset = new Point(e.getPoint().x, e.getPoint().y);
+                FilePanel.this.getParent().setComponentZOrder(FilePanel.this, 0);
+            }
+        });
+
+        this.addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                Point newLocation = e.getLocationOnScreen();
+                newLocation.translate(-mouseOffset.x, -mouseOffset.y);
+                
+                //align to nearest grid cell
+                int newX = (int) (Math.round((double) newLocation.x / GRID_CELL_SIZE) * GRID_CELL_SIZE);
+                int newY = (int) (Math.round((double) newLocation.y / GRID_CELL_SIZE) * GRID_CELL_SIZE);
+
+                FilePanel.this.setLocation(newX, newY);
+            }
+        });
     }
 
     static String parseFileName(String _filename) {
